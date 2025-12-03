0ca30553de025dda906ceea4a61b3ee5ad9cd287
AWAIS MAYO KING009 <rojxb268@gmail.com>
2025-12-03 07:26:46 +0500

    Add help command with bot menu and commands list

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,32 @@
+const config = require("../config");
 
+module.exports = {
+  name: "help",
+  run: async (bot, m, from) => {
+    bot.sendMessage(from, {
+      text: `✨ *${config.botName} MENU* ✨
+
+🟢 Basic Commands
+.alive
+.owner
+.hack
+.fun
+.logo text
+
+🔵 Group Commands
+.kick
+.add
+.promote
+.demote
+
+🟣 AI / Tools
+.ask
+.ss
+.sticker
+
++ 50 More Commands Included!
+
+📢 Channel: ${config.channel}`
+    });
+  }
+};
