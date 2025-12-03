2bf0a3bf110338ed6707dc580507e39499c1d5a7
AWAIS MAYO KING009 <rojxb268@gmail.com>
2025-12-03 07:18:06 +0500

    Add configuration for bot settings

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  owner: ["923295533214"],
+  botName: "AWAIS-HACKING-BOY",
+  channel: "https://whatsapp.com/channel/0029VbBzlMlIt5rzSeMBE922",
+  botdp: "https://ibb.co/nNJQH3sx",
+  prefix: "."
+};
